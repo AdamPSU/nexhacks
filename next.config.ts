6d46ad824406f2087220cf08bb05a18849627896
Adam T <art5809@psu.edu>
2026-01-17 13:44:59 -0500

    bun, next.js, shadcn ui configuration

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  serverExternalPackages: ['pino', 'pino-pretty', 'thread-stream'],
+};
+
+export default nextConfig;
+
+
+
+
+
